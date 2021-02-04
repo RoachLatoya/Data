554d0c45f3b6400c87daf33b2e7de468239e5415
loriannroach <loriann.roach@solutechinnovation.com>
2021-02-03 18:25:53 -0600

    Initial commit

---
+++ sum.js
@@ -0,0 +1,10 @@
+const MathOps= {
+ sum: function(a, b) {            
+   return a + b;
+ },
+ multiply: function(a,b)
+{
+  return a * b;
+}
+}
+module.exports = MathOps;
---
+++ sum.test.js
@@ -0,0 +1,10 @@
+const MathOps = require('./sum');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(MathOps.sum(1, 2)).toBe(3);
+});
+
+test('multiply 1 * 2 to equal 2', () => {
+expect(MathOps.multiply(1, 2)).toBe(2);
+ });
+                
