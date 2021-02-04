e5a41321285d3bec2f226b00cbda7e68852ffcb1
RoachLatoya <49956744+RoachLatoya@users.noreply.github.com>
2021-02-03 18:45:37 -0600

    Update sum.test.js

--- sum.test.js
+++ sum.test.js
@@ -4,7 +4,7 @@ test('adds 1 + 2 to equal 3', () => {
   expect(MathOps.sum(1, 2)).toBe(3);
 });
 
-test('multiply 1 * 2 to equal 2', () => {
+test('multiply 1 * 3 to equal 3', () => {
 expect(MathOps.multiply(1, 3)).toBe(3);
  });
                 
